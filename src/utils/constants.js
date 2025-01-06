0184da02083de493be06fe9bc47860e6fb6e9dfb
vishaljadhav7 <vishalj.7661@gmail.com>
2025-01-06 15:53:50 +0530

    new changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const HERO_URL = "https://images.unsplash.com/photo-1518315064-bec5daa6062b?q=80&w=1557&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
-export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "https://devdates-api-1.onrender.com"
 
